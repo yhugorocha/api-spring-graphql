5cf41b6c76928d872ab25d127e6c4b2782cd1fc5
Hugo Silva <yhugorocha@gmail.com>
2024-07-17 09:42:47 -0300

    Feat: Initial Commit.

---
+++ src/main/java/com/hugodev/ApiGraphQL/ApiGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.hugodev.ApiGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugodev/ApiGraphQL/ApiGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugodev.ApiGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
